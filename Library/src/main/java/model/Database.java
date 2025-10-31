ef7117fbb9ec3fc8245ed7dd9ac3c1f43747511b
kooditer <veiko.maesalu@gmail.com>
2025-10-31 10:21:51 +0200

    jdbc pooleli

--- Library/src/main/java/model/Database.java
+++ Library/src/main/java/model/Database.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
@@ -10,6 +12,19 @@ public class Database {
     public Database() {
         raamatud = new LinkedList<Raamat>();
     }
+
+    public void connect() throws Exception {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new Exception("Driver not found");
+        }
+        String url = "jdbc:mysql://localhost:3306/books";
+
+        Connection con = DriverManager.getConnection(url, "root", "root");
+    }
+
+    public void disconnect(){}
 
     public void addRaamat(Raamat raamat) {
         raamatud.add(raamat);
