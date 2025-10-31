12ea3208bd7fc5a61398608e48422dc2f9c5abd1
kooditer <veiko.maesalu@gmail.com>
2025-10-31 14:47:49 +0200

    Esimene jdbc lõpetatud

--- Library/src/main/java/model/Database.java
+++ Library/src/main/java/model/Database.java
@@ -2,29 +2,42 @@ package model;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
 public class Database {
     private List<Raamat> raamatud;
+    private Connection con;
 
     public Database() {
         raamatud = new LinkedList<Raamat>();
     }
 
     public void connect() throws Exception {
+        if (con!=null) return;
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException e) {
             throw new Exception("Driver not found");
         }
-        String url = "jdbc:mysql://localhost:3306/books";
+        String url = "jdbc:mysql://localhost:3306/sakila";
 
-        Connection con = DriverManager.getConnection(url, "root", "root");
+        con = DriverManager.getConnection(url, "root", "Veiko123");
+//        System.out.println("Connected: " + con);
     }
 
-    public void disconnect(){}
+    public void disconnect(){
+        if (con!=null) {
+            try {
+                con.close();
+            } catch (SQLException e) {
+                System.out.println("Ei saa ühendust sulgeda");
+//                throw new RuntimeException(e);
+            }
+        }
+    }
 
     public void addRaamat(Raamat raamat) {
         raamatud.add(raamat);
