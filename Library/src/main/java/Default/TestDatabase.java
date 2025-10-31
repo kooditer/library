620f1be89d84806ffcdaffbaf673fa291bb22e4c
kooditer <veiko.maesalu@gmail.com>
2025-10-31 15:06:23 +0200

    5harjutus jdbc esimene peatükk tehtud

---
+++ Library/src/main/java/Default/TestDatabase.java
@@ -0,0 +1,20 @@
+package Default;
+
+import model.Database;
+
+public class TestDatabase {
+    public static void main(String[] args) {
+        System.out.println("Running database test...");
+
+        Database db = new Database();
+
+        try {
+            db.connect();
+        } catch (Exception e) {
+//            throw new RuntimeException(e);
+            e.printStackTrace();
+        }
+        db.disconnect();
+    }
+
+}
--- Library/src/main/java/model/Database.java
+++ Library/src/main/java/model/Database.java
@@ -1,14 +1,40 @@
 package model;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
 public class Database {
     private List<Raamat> raamatud;
+    private Connection con;
 
     public Database() {
         raamatud = new LinkedList<Raamat>();
+    }
+
+    public void connect() throws Exception{
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new Exception("Driver not found");
+        }
+        String url = "jdbc:mysql://localhost:3306/sakila";
+        con = DriverManager.getConnection(url, "root", "Veiko123");
+    }
+
+    public void disconnect(){
+        if (con!=null) {
+            try {
+                con.close();
+            } catch (SQLException e) {
+//                throw new RuntimeException(e);
+                System.out.println("Cant close connection");
+            }
+        }
+
     }
 
     public void addRaamat(Raamat raamat) {
